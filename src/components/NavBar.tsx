ddc87adf51c2f5495f97ef5f1743ecfe5d2ed0cb
OriginalCola <142392135+OriginalCola@users.noreply.github.com>
2023-09-26 17:12:11 +0200

    updates in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,19 +7,21 @@ import Image from 'next/image'
 
 export const NavBar = () => {
     return (
-        <div className="flex flex-row justify-between items-center p-2.5">
-            <div>
-                <h1>HumblyGambly</h1>
-            </div>
-            <div className="flex justify-between">
-                <div className="border-red-600 border-2 border-solid flex items-center">
-                    <ul className="flex items-center uppercase space-x-4">
-                        <li className="">cases</li>
-                        <li className="">upgrader</li>
-                        <li className="">roulette</li>
-                    </ul>
+        <div className="flex flex-row justify-between items-center p-2.5 border border-astronaut border-solid">
+            <div className="flex items-center w-2/5 space-x-5">
+                <div>
+                    <h1>HumblyGambly</h1>
                 </div>
-                <div className="border-green-600 border-2 border-solid flex flex-row space-x-4">
+                <div className="border-red-600 border-2 border-solid flex items-center">
+                        <ul className="flex items-center uppercase space-x-4">
+                            <li className="">cases</li>
+                            <li className="">upgrader</li>
+                            <li className="">roulette</li>
+                        </ul>
+                </div>
+            </div>
+            <div className="flex items-center space-x-7 h-full">
+                <div className="border-green-600 border-2 border-solid flex flex-row space-x-4 flex items-center justify-between space-x-5">
                     <h2>DropKing</h2>
                     <div className="avatar">
                         <div className="w-10 rounded-full ring ring-offset-base-100 ring-offset-2">
@@ -31,12 +33,24 @@ export const NavBar = () => {
                             />
                         </div>
                     </div>
-                    <h2>5,56 $</h2>
+                    <h2 className="text-jaffa">5,56 $</h2>
                     <button>deposit</button>
                 </div>
-            </div>
-            <div className="flex flex-row">
-                <FontAwesomeIcon icon={faVolumeHigh} />
+                <div className='border border-l-astronaut border-solid h-full w-1'>
+
+                </div>
+                <div className="flex items-center space-x-2">
+                    <div className='space-x-5'>
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                            <div className="dropdown dropdown-end">
+                                <label tabIndex={0} className="btn m-1">ENG</label>
+                                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li><a>English</a></li>
+                                    <li><a>Polish</a></li>
+                                </ul>
+                            </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
