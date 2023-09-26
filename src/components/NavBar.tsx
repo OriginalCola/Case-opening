3e9425ce47ffc2e6deba84373b1e293efe7de280
OriginalCola <142392135+OriginalCola@users.noreply.github.com>
2023-09-27 00:10:06 +0200

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,37 @@
+'use client';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faVolumeHigh
+  faVolumeHigh,
+  faWallet,
+  faClover
 } from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
-
+import Link from 'next/link'
 
 export const NavBar = () => {
+
+    function DepositButton() {
+        window.focus();
+        confirm('1000$ deposited');
+    }
+
     return (
         <div className="flex flex-row justify-between items-center p-2.5 border border-astronaut border-solid">
-            <div className="flex items-center w-2/5 space-x-5">
+            <div className="flex items-center w-2/5 space-x-5 ">
                 <div>
-                    <h1>HumblyGambly</h1>
+                    <h1 className="font-extrabold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">HumblyGambly</h1>
+                    <FontAwesomeIcon icon={faClover} />
                 </div>
-                <div className="border-red-600 border-2 border-solid flex items-center">
+                <div className="flex items-center">
                         <ul className="flex items-center uppercase space-x-4">
-                            <li className="">cases</li>
-                            <li className="">upgrader</li>
-                            <li className="">roulette</li>
+                            <li>
+                                <Link href="/">Deposit</Link>
+                            </li>
                         </ul>
                 </div>
             </div>
             <div className="flex items-center space-x-7 h-full">
-                <div className="border-green-600 border-2 border-solid flex flex-row space-x-4 flex items-center justify-between space-x-5">
+                <div className="flex flex-row space-x-4 flex items-center justify-between space-x-5">
                     <h2>DropKing</h2>
                     <div className="avatar">
                         <div className="w-10 rounded-full ring ring-offset-base-100 ring-offset-2">
@@ -34,7 +44,10 @@ export const NavBar = () => {
                         </div>
                     </div>
                     <h2 className="text-jaffa">5,56 $</h2>
-                    <button>deposit</button>
+                    <button className="btn" onClick={DepositButton}>
+                    Deposit
+                    <FontAwesomeIcon icon={faWallet} />
+                    </button>
                 </div>
                 <div className='border border-l-astronaut border-solid h-full w-1'>
 
